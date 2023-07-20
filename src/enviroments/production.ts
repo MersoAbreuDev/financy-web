e766ba43b874c501395cd3348c6f5674a904a951
DevMersoAbreu <emerson.solutions@outlook.com>
2023-07-19 21:56:40 -0300

    firebase

---
+++ src/enviroments/production.ts
@@ -0,0 +1,1 @@
+export const SERVER_URI = "https://financy-ccbb427a7556.herokuapp.com/financy/"
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-
+import { firebaseConfig } from './firebase-config'; // Import the firebase-config.ts file
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
